fc3311b56c30ca40a3859b781d4bb5974270adea
JoaKam <joannakamienik@wp.pl>
2017-11-07 21:51:23 +0100

    Exercise 13.3 - problems

---
+++ src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -0,0 +1,53 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Table
+public class Invoice {
+    private int id;
+    private String number;
+    private List<Item> items;
+
+    public Invoice() {
+    }
+
+    public Invoice(int id, String number, List<Item> items) {
+        this.id = id;
+        this.number = number;
+        this.items = items;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "NUMBER")
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "ITEMS")
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
---
+++ src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,55 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Product prodApple = new Product(1, "Apple");
+        Product prodBanana = new Product(2, "Banana");
+        Product prodOrange = new Product(3, "Orange");
+
+        Item itemApple = new Item(5, prodApple, new BigDecimal(5), 4, new BigDecimal(20));
+        Item itemApple2 = new Item(5, prodApple, new BigDecimal(6), 3, new BigDecimal(18));
+        Item itemBanana = new Item(5, prodBanana, new BigDecimal(4), 2, new BigDecimal(8));
+        Item itemOrange = new Item(5, prodOrange, new BigDecimal(3), 1, new BigDecimal(3));
+
+        Invoice invoiceAppleApple2 = new Invoice(0, "1/11/2017", Arrays.asList(itemApple, itemApple2));
+        Invoice invoiceAppleOrangeBanana = new Invoice(2, "1/11/2017", Arrays.asList(itemApple, itemOrange, itemBanana));
+
+        prodApple.setItems(Arrays.asList(itemApple, itemApple2));
+        prodBanana.setItems(Arrays.asList(itemBanana));
+        prodOrange.setItems(Arrays.asList(itemOrange));
+
+        //When
+        invoiceDao.save(invoiceAppleApple2);
+        invoiceDao.save(invoiceAppleOrangeBanana);
+        int idInvAppleApple2 = invoiceAppleApple2.getId();
+        int idInvAppleOrangeBanana = invoiceAppleOrangeBanana.getId();
+
+        //Then
+        Assert.assertNotEquals(0, idInvAppleApple2);
+        Assert.assertNotEquals(2, idInvAppleOrangeBanana);
+
+        //CleanUp
+        invoiceDao.delete(idInvAppleApple2);
+        invoiceDao.delete(idInvAppleOrangeBanana);
+    }
+}
