e8033dd5837ceee43d98f4288540d42e0241aa74
JoaKam <joannakamienik@wp.pl>
2017-11-27 00:20:30 +0100

    Exercise 20.1

---
+++ src/test/java/com/kodilla/hibernate/manytomany/dao/facade/SearchFacadeTestSuite.java
@@ -0,0 +1,64 @@
+package com.kodilla.hibernate.manytomany.dao.facade;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import com.kodilla.hibernate.manytomany.dao.CompanyDao;
+import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
+import com.kodilla.hibernate.manytomany.facade.api.SearchFacade;
+import com.kodilla.hibernate.manytomany.facade.api.SearchingException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchFacadeTestSuite {
+    @Autowired
+    CompanyDao companyDao;
+    @Autowired
+    EmployeeDao employeeDao;
+    @Autowired
+    SearchFacade searchFacade;
+
+    @Test
+    public void testSearchFacade() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //When
+
+        try {
+            searchFacade.processSearchCompany("%ter%");
+        } catch (SearchingException e) {
+
+        }
+
+        try {
+            searchFacade.processSearchEmployee("%l%");
+        } catch (SearchingException e) {
+
+        }
+    }
+}
