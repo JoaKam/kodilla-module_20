a8cc55cfd6262a6f0f2300b348ed53325b04d629
JoaKam <joannakamienik@wp.pl>
2017-11-27 20:35:11 +0100

    Exercise 20.2

--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/decorator/pizza/ExtraCheesePizzaOrderDecorator.java
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/decorator/pizza/ExtraCheesePizzaOrderDecorator.java
@@ -2,18 +2,18 @@ package com.kodilla.kodillapatterns2.decorator.pizza;
 
 import java.math.BigDecimal;
 
-public class ExtraCheesePizzaOrderDecorator extends AbstractPizzaOrderDecorator{
-    public ExtraCheesePizzaOrderDecorator(PizzaOrder pizzaOrder){
+public class ExtraCheesePizzaOrderDecorator extends AbstractPizzaOrderDecorator {
+    public ExtraCheesePizzaOrderDecorator(PizzaOrder pizzaOrder) {
         super(pizzaOrder);
     }
 
     @Override
-    public BigDecimal getCost(){
+    public BigDecimal getCost() {
         return super.getCost().add(new BigDecimal(5));
     }
 
     @Override
-    public String getDesc(){
+    public String getDesc() {
         return super.getDesc() + " with extra cheese";
     }
 }
--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/decorator/pizza/ThickPizzaOrderDecorator.java
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/decorator/pizza/ThickPizzaOrderDecorator.java
@@ -2,18 +2,18 @@ package com.kodilla.kodillapatterns2.decorator.pizza;
 
 import java.math.BigDecimal;
 
-public class ThickPizzaOrderDecorator extends AbstractPizzaOrderDecorator{
-    public ThickPizzaOrderDecorator(PizzaOrder pizzaOrder){
+public class ThickPizzaOrderDecorator extends AbstractPizzaOrderDecorator {
+    public ThickPizzaOrderDecorator(PizzaOrder pizzaOrder) {
         super(pizzaOrder);
     }
 
     @Override
-    public BigDecimal getCost(){
+    public BigDecimal getCost() {
         return super.getCost().add(new BigDecimal(2));
     }
 
     @Override
-    public String getDesc(){
+    public String getDesc() {
         return super.getDesc() + " thick crust";
     }
 }
